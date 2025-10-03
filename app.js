2238b4cfaeb6da8c2a711090ac10dd7b898d1804
natehog233 <natehog233@gmail.com>
2025-10-02 22:43:03 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,1 @@
+// Intentionally empty; logic is embedded in index.html for a single-file deploy.
---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'liftoff-lite-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js', // not used, but kept in case of future split
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
